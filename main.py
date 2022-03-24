3fe3f6ded1158d5d139392f2e8928b26b429fc8c
Manas Paranjape <mparanja@purdue.edu>
2022-03-24 18:13:29 -0400

    Check

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tensorflow.keras import datasets, layers, Sequential, models
 
 print(tf.__version__)
 
+pixels = 1024
+
 model = models.Sequential()
 model.add(layers.Conv2D(pixels, (3, 3), activation='relu', input_shape=(pixels, pixels, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
