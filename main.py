9b3511fc8885fbddd4461e3e51be86bc92399fd3
Manas Paranjape <mparanja@purdue.edu>
2022-03-24 23:50:22 -0400

    adds

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from keras import Sequential, datasets, layers, models
 train_data_points = 1000
 test_data_points = 100
 total_data_points = train_data_points
-pixels = 1024
+pixels = 500
+dimensions = (pixels, pixels)
 bins = 3
 mid_layer = 750
 training_iters = 10
@@ -17,36 +18,57 @@ learning_rate = 2e-3
 num_epochs = 1
 
 train_images = []
-train_labels = np.array()
+train_labels = np.array([])
 folder_path = "/mnt/c/NN/Pneumonia/chest_xray/train/"
 
 for f in listdir(folder_path + "NORMAL"):
-    im = img.open(f)
+    im = img.open(folder_path + "NORMAL/" + f)
+    im = im.resize(dimensions)
+    im = im.convert('L')
+    im_array = np.asarray(im)
+    train_images.append(im_array)
+    im.close()
     train_images.append(im)
     train_labels = np.append(train_labels, 0)
 
 for f in listdir(folder_path + "PNEUMONIA"):
-    im = img.open(f)
-    train_images.append(im)
+    im = img.open(folder_path + "PNEUMONIA/" + f)
+    im = im.resize(dimensions)
+    im = im.convert('L')
+    im_array = np.asarray(im)
+    train_images.append(im_array)
+    im.close()    
     if "bacteria" in f:
         train_labels = np.append(train_labels, 1)
     elif "virus" in f:
         train_labels = np.append(train_labels, 2)
 
 test_images = []
-test_labels = np.array()
+test_labels = np.array([])
 folder_path = "/mnt/c/NN/Pneumonia/chest_xray/test/"
 
 for f in listdir(folder_path + "NORMAL"):
-    im = img.open(f)
-    test_images.append(im)
+    im = img.open(folder_path + "NORMAL/" + f)
+    im = im.resize(dimensions)
+    im = im.convert('L')
+    im_array = np.asarray(im)
+    test_images.append(im_array)
+    im.close()    
     test_labels = np.append(test_labels, 0)
 
 for f in listdir(folder_path + "PNEUMONIA"):
-    im = img.open(f)
-    test_images.append(im)
+    im = img.open(folder_path + "PNEUMONIA/" + f)
+    im = im.resize(dimensions)
+    im = im.convert('L')
+    im_array = np.asarray(im)
+    test_images.append(im_array)
+    im.close()    
     if "bacteria" in f:
         test_labels = np.append(test_labels, 1)
     elif "virus" in f:
         test_labels = np.append(test_labels, 2)
 
+train_images = np.array(train_images)
+test_images = np.array(test_images)
+print(np.shape(train_images))
+print(np.array(test_images))
