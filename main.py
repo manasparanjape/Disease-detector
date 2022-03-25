ae540faa7d501cc7c7b53991dab70065fbc5243a
Manas Paranjape <mparanja@purdue.edu>
2022-03-24 21:31:55 -0400

    Some code

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, Sequential, models
+from os import listdir
+from os.path import isfile, join
+from PIL import Image as img
+import glob
 
 print(tf.__version__)
 
+train_data_points = 1000
+test_data_points = 100
+total_data_points = train_data_points
 pixels = 1024
+bins = 3
+mid_layer = 750
+training_iters = 10
+learning_rate = 2e-3
+num_epochs = 1
 
-model = models.Sequential()
-model.add(layers.Conv2D(pixels, (3, 3), activation='relu', input_shape=(pixels, pixels, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(2 * pixels, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(2 * pixels, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+train_images = []
+train_labels = np.array()
+folder_path = "C:/NN/Pneumonia/chest_xray/train/"
+for filename in glob.glob(folder_path + "NORMAL"):
+    
