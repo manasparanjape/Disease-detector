3d206e9336822b010151de394d212a7b893d72a4
Manas Paranjape <mparanja@purdue.edu>
2022-05-29 12:38:30 -0500

    changes

--- project/image/models.py
+++ project/image/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
 DISEASE_CHOICES = [
-    ('bt', 'Brain Tumor'),
-    ('l', 'Lukemia'),
-    ('p', 'Pneumonia'),
+    ('bt', 'Brain Tumor (MRI scan of brain of Patient)'),
+    ('l', 'Acute Lymphoblastic Lukemia '
+          '(Image of bone marrow cell of patient)'),
+    ('p', 'Pneumonia (Chest x-ray of patient)'),
 ]
+
 
 # Create your models here.
 class Image(models.Model):
-    image=models.ImageField(upload_to="img/%y")
-    disease=models.CharField(max_length=2, choices=DISEASE_CHOICES, default='BT')
+    image = models.ImageField(upload_to="img/%y")
+    disease = models.CharField(
+        max_length=2, choices=DISEASE_CHOICES, default='BT')
+
     def __str__(self):
         return f'{self.disease}'
