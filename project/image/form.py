8218c8256f39a04d88d3521efc7005a9986fde8f
Manas Paranjape <mparanja@purdue.edu>
2022-05-29 12:38:22 -0500

    changes

--- project/image/form.py
+++ project/image/form.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Image
 
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('image', 'disease')
+
+    def __init__(self, *args, **kwargs):
+        super(ImageForm, self).__init__(*args, **kwargs)
+        self.fields['image'].widget.attrs.update({'class': 'uploading'})
+        self.fields['disease'].widget.attrs.update({'class': 'disease'})
